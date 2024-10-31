130f493dd79b8a403a2d2bf0641b430603936e05
Mayenziwe <mayenziwemaseko06@gmail.com>
2024-10-31 17:24:47 +0200

    Search results displaying properly

--- public/search.php
+++ public/search.php
@@ -21,22 +21,27 @@ if (isset($_GET['search'])) {
     ?>
 
     <main>
-        <?php
-        if (isset($result) && mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-        ?>
-                <div class="card col-sm-6 col-lg-4 mt-5 ms-1 me-1" style="width: 18rem;">
-                    <img src="<?php echo $row['image']; ?>" class="card-img-top item-display-image" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title fw-bold" style="color: #D20000;"><?php echo $row['name']; ?></h5>
-                        <h6 class="fst-italic">E<?php echo $row['price'] ?></h6>
-                        <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
-                        <a href="#" class="btn btn-primary">Add to cart</a>
-                    </div>
-                </div>
-        <?php
-            }
-        }
-        mysqli_close($conn);
-        ?>
+        <div class="container-sm">
+        <span class="fw-bold h1" style="color: #D20000;">Search results for "<?php echo htmlspecialchars($searchValue) ?>" </span>
+            <div class="row card-deck justify-content-center">
+                <?php
+                if (isset($result) && mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+                        <div class="card col-sm-6 col-lg-4 mt-5 ms-1 me-1" style="width: 18rem;">
+                            <img src="<?php echo htmlspecialchars($row['image']); ?>" class="card-img-top item-display-image" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold" style="color: #D20000;"><?php echo htmlspecialchars($row['name']); ?></h5>
+                                <h6 class="fst-italic">E<?php echo htmlspecialchars($row['price']) ?></h6>
+                                <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                                <a href="#" class="btn btn-primary">Add to cart</a>
+                            </div>
+                        </div>
+                <?php
+                    }
+                }
+                mysqli_close($conn);
+                ?>
+            </div>
+        </div>
     </main>

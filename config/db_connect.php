8bcf4e9a2f21b6d71b44a144de93cb35ec7274fa
Mayenziwe <mayenziwemaseko06@gmail.com>
2024-11-02 12:36:29 +0200

    Renamed database

--- config/db_connect.php
+++ config/db_connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$db_name = "refectory-food-system";
+$db_name = "refectory";
 
 $conn = mysqli_connect($host, $username, $password, $db_name);
 
